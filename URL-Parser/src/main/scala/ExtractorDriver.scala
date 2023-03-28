0c6a48a96777cd42cc9f529bbe9a5ea656381813
Tushar Dhiman <tushar.dhiman@knoldus.com>
2023-03-28 16:15:39 +0530

    Updated File

--- URL-Parser/src/main/scala/ExtractorDriver.scala
+++ URL-Parser/src/main/scala/ExtractorDriver.scala
@@ -3,7 +3,6 @@ package com.knoldus.urlparser
 object ExtractorDriver extends App {
 
   private val urlParserObject = UrlParser("https://www.mywebsite.com/home")
-
   urlParserObject match {
     case UrlParser(protocol, domain, path) => println(s"Protocol:$protocol\nDomain:$domain\nPath:$path")
     case _ => println("Not a valid URL")
--- URL-Parser/src/main/scala/UrlParser.scala
+++ URL-Parser/src/main/scala/UrlParser.scala
@@ -1,10 +1,12 @@
 package com.knoldus.urlparser
+
 import scala.util.Try
 
 object UrlParser {
-
+  //This method takes a string URL and returns it as is
   def apply(url: String) = s"$url"
 
+  // This method extracts the protocol, domain, and path from a string URL
   def unapply(url: String): Option[(String, String, String)] = {
 
     Try {
